eb8cfae55b052653f71ea0b18e0a092ba1239017
Alan <alanjmurphy91@gmail.com>
2018-08-12 14:51:23 +0100

    test client-side routing

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import {BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom';
 import Header from '.././components/Header';
+import PortfolioHome from '.././components/PortfolioHome';
+import Portfolio from '.././components/Portfolio';
+import Contact from '.././components/Contact';
 import NotFoundPage from '.././components/NotFoundPage';
 
-const AppRouter = () => (
-    <BrowserRouter>
-        <div>
-        <Header />
-        <Switch>
-            <Route path="/" component={ExpenseDashboardPage} exact/>
-            <Route path="/create" component={AddExpensePage}/>
-            <Route path="/edit/:id" component={EditExpensePage}/>
-            <Route path="/help" component={HelpPage}/>
-            <Route component={NotFoundPage}/>
-        </Switch>
-        </div>
-    </BrowserRouter>
-);
+const AppRouter = () => {
+    return (
+        <BrowserRouter>
+            <div>
+                <Header/>
+                <Switch>
+                    <Route exact path="/" component={PortfolioHome} exact/>
+                    <Route exact path="/portfolio/:id" component={Portfolio}/>
+                    <Route exact path="/contact" component={Contact}/>
+                    <Route component={NotFoundPage}/>
+                </Switch>
+            </div>
+        </BrowserRouter>
+    );
+}
 
 export default AppRouter
